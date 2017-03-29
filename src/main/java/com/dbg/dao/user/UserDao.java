9d3064a753f8a4f111eaf8b736d468e27e0c5820
aleja <aleja@192.168.1.107>
2017-03-30 00:54:32 +0200

    UserDao

---
+++ src/main/java/com/dbg/dao/user/UserDao.java
@@ -0,0 +1,14 @@
+package com.dbg.dao.user;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dbg.model.user.User;
+
+
+@Repository
+public interface UserDao extends PagingAndSortingRepository<User,Integer>{
+	public List<User> findNameLike(String name);
+}
